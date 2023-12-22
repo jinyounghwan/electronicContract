12a2d881448407385bb2a430ebf8baa16f35246b
DESKTOP-IUKFS8F\dejay <hsk9839@dejay.co.kr>
2023-12-22 14:08:56 +0900

    File , FileVO 필드 통일

--- src/main/java/com/samsung/framework/domain/file/File.java
+++ src/main/java/com/samsung/framework/domain/file/File.java
@@ -13,8 +13,8 @@ public class File {
     // 게시판 SEQ
     private String boardSeq;
 
-    // 파일 크기
-    private Long fileSize;
+    // 파일 용량
+    private String fileSize;
 
     //파일 번호
     private Long fileNo;
@@ -34,9 +34,15 @@ public class File {
     // 삭제 여부
     private String delYn;
 
+    // 파일 유형 01:이미지, 02:비디오, 03:엑셀, 04:워드, 05:한글, 06: 그 외 문
+    private String fileType;
+
     // 등록일
     private String regDt;
 
     // 등록자
     private String regId;
+
+    // 마지막 등록일
+    private String lastId;
 }
--- src/main/java/com/samsung/framework/vo/file/FileVO.java
+++ src/main/java/com/samsung/framework/vo/file/FileVO.java
@@ -9,30 +9,42 @@ import lombok.*;
 @Builder
 public class FileVO {
     // 파일 SEQ
-    private Long fileSeq;
-    
-    // 테이블 명
-    private String entityName;
+    private  Long fileSeq;
 
-    // 테이블 SEQ
-    private Long entitySeq;
+    // 게시판 SEQ
+    private String boardSeq;
 
-    // 테이블 TYPE
-    private String entityType;
+    // 파일 용량
+    private String fileSize;
+
+    //파일 번호
+    private Long fileNo;
 
     // 파일 경로
     private String filePath;
-    
+
     // 파일 이름
-    private String fileName;
+    private String fileNm;
 
     // 기존 파일 명
-    private String originFileName;
+    private String fileNmOrg;
+
+    // 썸네일 여부
+    private String thumbnailYn;
+
+    // 삭제 여부
+    private String delYn;
 
     // 파일 유형 01:이미지, 02:비디오, 03:엑셀, 04:워드, 05:한글, 06: 그 외 문
     private String fileType;
 
-    // 파일 사이즈
-    private Long fileSize;
+    // 등록일
+    private String regDt;
+
+    // 등록자
+    private String regId;
+
+    // 마지막 등록일
+    private String lastId;
 
 }
