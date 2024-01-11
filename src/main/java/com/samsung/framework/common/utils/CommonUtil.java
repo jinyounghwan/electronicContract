82afc55aff552c684525f431cd4cae0574dd8b91
ijzone <ijzone@dejay.co.kr>
2024-01-11 13:43:05 +0900

    [Refactor] 미사용 패키지 정리

--- src/main/java/com/samsung/framework/common/utils/CommonUtil.java
+++ src/main/java/com/samsung/framework/common/utils/CommonUtil.java
@@ -11,18 +11,18 @@ import org.springframework.stereotype.Component;
 public class CommonUtil {
 
 //    private final TokenFactory tokenFactory;
-    private final SessionFactory sessionFactory;
-    private final CookieFactory cookieFactory;
+//    private final SessionFactory sessionFactory;
+//    private final CookieFactory cookieFactory;
 
 //    public TokenFactory getTokenFactory() {
 //        return this.tokenFactory;
 //    }
 
-    public SessionFactory getSessionFactory() {
-        return this.sessionFactory;
-    }
+//    public SessionFactory getSessionFactory() {
+//        return this.sessionFactory;
+//    }
 
-    public CookieFactory getCookieFactory() {
-        return this.cookieFactory;
-    }
+//    public CookieFactory getCookieFactory() {
+//        return this.cookieFactory;
+//    }
 }
