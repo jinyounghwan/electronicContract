1beeae14431748ec7c6a69a9e179878fedc08c31
ijzone <ijzone@dejay.co.kr>
2024-01-15 09:25:04 +0900

    [로그] 로그저장 API 샘플

--- src/main/java/com/samsung/framework/IndexController.java
+++ src/main/java/com/samsung/framework/IndexController.java
@@ -1,6 +1,11 @@
 package com.samsung.framework;
 
+import com.samsung.framework.common.enums.LogTypeEnum;
+import com.samsung.framework.common.utils.LogUtil;
+import com.samsung.framework.domain.log.LogSaveRequest;
+import com.samsung.framework.vo.log.LogSaveResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,15 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/index")
 @RestController
 public class IndexController {
 
     private static final String content = "%s";
     private final AtomicInteger counter = new AtomicInteger();
+
+    private final LogUtil logUtil;
 
     @GetMapping({"", "/"})
     public ResponseEntity index() {
@@ -38,9 +47,22 @@ public class IndexController {
         log.info("REDIRECT TEST");
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(URI.create("/member"));
+
         return new ResponseEntity(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 
+    @GetMapping("/saveLog")
+    public ResponseEntity saveLog(HttpServletRequest request) {
+        var logSaveRequest = LogSaveRequest.builder()
+                .logType(LogTypeEnum.LOGIN)
+                .ipAddress(request.getRemoteAddr() + ":" + request.getRemotePort())
+                .createdBy("ikjoo Lee")
+                .build();
+
+        Map<String, LogSaveResponse> resultMap = logUtil.saveLog(logSaveRequest);
+
+        return ResponseEntity.ok().body(resultMap);
+    }
 }
 
 
