fc5d067d13481ad7c857ad9b62d9cdc2649ce0ad
ijzone <ijzone@dejay.co.kr>
2024-01-15 16:02:49 +0900

    [패키지] 계약 시스템 base 패키지 & 컨트롤러 추가

---
+++ src/main/java/com/samsung/framework/controller/account/AccountController.java
@@ -0,0 +1,17 @@
+package com.samsung.framework.controller.account;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 계정 관련 컨트롤러
+ */
+@Slf4j
+@RequestMapping("/account")
+@Controller
+public class AccountController {
+
+    public AccountController() {
+    }
+}
---
+++ src/main/java/com/samsung/framework/controller/contract/documented/ContractSearchController.java
@@ -0,0 +1,17 @@
+package com.samsung.framework.controller.contract.documented;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 계약서 조회 관련 컨트롤러
+ */
+@Slf4j
+@RequestMapping("/contract/search")
+@Controller
+public class ContractSearchController {
+
+    public ContractSearchController() {
+    }
+}
