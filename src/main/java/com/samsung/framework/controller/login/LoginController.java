7e57419e2c7d6de14b6c756f9e14088d2cbc2bd4
Seounggyun <hsk9839@dejay.co.kr>
2024-01-15 17:26:53 +0900

    [Log] 로그인 시, log저장

--- src/main/java/com/samsung/framework/controller/login/LoginController.java
+++ src/main/java/com/samsung/framework/controller/login/LoginController.java
@@ -1,10 +1,14 @@
 package com.samsung.framework.controller.login;
 
 import com.samsung.framework.common.enums.ExceptionCodeMsgEnum;
+import com.samsung.framework.common.enums.LogTypeEnum;
 import com.samsung.framework.common.exception.CustomLoginException;
+import com.samsung.framework.common.utils.LogUtil;
 import com.samsung.framework.controller.common.ParentController;
+import com.samsung.framework.domain.log.LogSaveRequest;
 import com.samsung.framework.domain.member.LoginRequest;
 import com.samsung.framework.service.member.MemberService;
+import com.samsung.framework.vo.log.LogSaveResponse;
 import com.samsung.framework.vo.member.MemberVO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -16,12 +20,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/login")
 public class LoginController{
     private final MemberService memberService;
+    private final LogUtil logUtil;
     /**
      * login view를 가져와 준다.
      * @return
@@ -49,7 +56,14 @@ public class LoginController{
             session.setAttribute("loginInfo", loginInfo);
             session.setMaxInactiveInterval(1800);
 
-            return new ResponseEntity(loginInfo, HttpStatus.OK);
+            var logSaveRequest = LogSaveRequest.builder()
+                    .logType(LogTypeEnum.LOGIN)
+                    .ipAddress(request.getRemoteAddr() + ":" + request.getRemoteAddr())
+                    .createdBy(String.valueOf(loginInfo.getEmpNo()))
+                    .build();
+
+            Map<String, LogSaveResponse> resultMap = logUtil.saveLog(logSaveRequest);
+            return ResponseEntity.ok().body(resultMap);
         }else {
             session.setAttribute("loginInfo", null);
 
