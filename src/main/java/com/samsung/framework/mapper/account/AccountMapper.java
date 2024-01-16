1478c58b06559a628f0ba0a2984273e73410be74
Seounggyun <hsk9839@dejay.co.kr>
2024-01-16 18:39:22 +0900

    @MapperAnnotation 추가 ,  getMemberList ADMIN_ID 컬럼 추가

--- src/main/java/com/samsung/framework/mapper/account/AccountMapper.java
+++ src/main/java/com/samsung/framework/mapper/account/AccountMapper.java
@@ -3,16 +3,21 @@ package com.samsung.framework.mapper.account;
 import com.samsung.framework.domain.account.Account;
 import com.samsung.framework.vo.account.AccountVO;
 import com.samsung.framework.vo.search.SearchVO;
+import com.samsung.framework.vo.search.account.AccountSearchVO;
+import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 
+@Mapper
 public interface AccountMapper {
+    int findAllTotalCount(SearchVO searchVO);
     int memberListCount();
     int memberListSearchCount(SearchVO search);
     List<AccountVO> getMemberList(SearchVO searchObject);
     AccountVO findMemberById(int id);
     int insertMember(AccountVO account);
     AccountVO findMemberByUserName(String userName);
-    AccountVO getLoginInfo(String userName);
+    AccountVO getLoginInfo(String userId);
     int updatePwd(Account member);
     boolean existsByEmpNo(int empNo);
     /**
