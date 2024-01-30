bb248bf3ab06d3410bc430de202ff31b4a1635c6
ijzone <ijzone@dejay.co.kr>
2024-01-30 17:49:06 +0900

    [Refactor] 미사용 패키지 및 코드 정리

--- src/main/java/com/samsung/framework/service/menu/MenuService.java
+++ src/main/java/com/samsung/framework/service/menu/MenuService.java
@@ -1,12 +1,8 @@
 package com.samsung.framework.service.menu;
 
-import com.samsung.framework.common.enums.RequestTypeEnum;
-import com.samsung.framework.common.enums.TableNameEnum;
 import com.samsung.framework.domain.account.SignUpRequest;
-import com.samsung.framework.domain.menu.Menu;
 import com.samsung.framework.mapper.authority.AuthorityMapper;
 import com.samsung.framework.mapper.menu.MenuMapper;
-import com.samsung.framework.vo.account.AccountVO;
 import com.samsung.framework.vo.authority.MenuAuthorityVO;
 import com.samsung.framework.vo.menu.MenuVO;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,49 +25,6 @@ public class MenuService{
                                                             add("MENU105000");
                                                         }
                                                     };
-
-    /**
-     * 메뉴 삽입
-     * @param menu
-     * @return 삽입 성공 1, 삽입 실패 0
-     */
-    public int insertMenu(Menu menu, AccountVO member){
-        Menu target = Menu.builder()
-                .menuCode(menu.getMenuCode())
-                .name(menu.getName())
-                .ord(menu.getOrd())
-                .displayYn(menu.getDisplayYn())
-                .useYn(menu.getUseYn())
-                .tableName(TableNameEnum.MENU.name())
-                .logId1("")
-                .logType(RequestTypeEnum.CREATE.getRequestType())
-                .logId2(null)
-                .logJson(null)
-                .remark(null)
-                .regId(member.getUserId())
-                .build();
-
-        int iAffectedRows = menuMapper.insert(target);
-
-        return iAffectedRows;
-    }
-
-
-    /**
-     * 메뉴 최대 ORD +1 값 조회
-     * @param pMenuSeq
-     * @return 최대 순서
-     */
-    public Long getOrdMax(Long pMenuSeq) {
-        return menuMapper.getOrdMax(pMenuSeq);
-    }
-
-    /**
-     * 메뉴 리스트 조회
-     */
-    public List<MenuVO> getMenuList(){
-        return menuMapper.getMenuList();
-    }
 
     /**
      * 대메뉴 리스트 조회
