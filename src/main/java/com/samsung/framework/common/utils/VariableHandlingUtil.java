e96b465dfcd4ee0f09d0c4708e8073fb9afb231d
ijzone <ijzone@dejay.co.kr>
2024-01-30 15:50:13 +0900

    [개발] 계약서 변수 치환 메서드 개선

--- src/main/java/com/samsung/framework/common/utils/VariableHandlingUtil.java
+++ src/main/java/com/samsung/framework/common/utils/VariableHandlingUtil.java
@@ -1,9 +1,12 @@
 package com.samsung.framework.common.utils;
 
-import com.samsung.framework.common.enums.ContractVariableEnum;
 import com.samsung.framework.domain.common.Variables;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import java.lang.reflect.Field;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Slf4j
@@ -17,27 +20,44 @@ public class VariableHandlingUtil {
      * @return contents {@link String}
      */
     public String replaceVariables(String contents, Variables target) {
+        Field[] declaredFields = target.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+            field.setAccessible(true);
+            Object value;
 
-        for (ContractVariableEnum var : ContractVariableEnum.values()) {
-            switch(var) {
-                case NAME -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getName());
-                case EMPLOYEE_NO -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getEmployeeNo());
-                case HIRE_DATE_HU -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getHireDateHu());
-                case HIRE_DATE_EN -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getHireDateEn());
-                case CONTRACT_DATE_HU -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getContractDateHu());
-                case CONTRACT_DATE_EN -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getContractDateEn());
-                case JOB_TITLE_HU -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getJobTitleHu());
-                case JOB_TITLE_EN -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getJobTitleEn());
-                case SALARY_NO -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getSalaryNo());
-                case SALARY_HU -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getSalaryHu());
-                case SALARY_EN -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getSalaryEn());
-                case WAGE_TYPE_EN -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getWageTypeEn());
-                case WAGE_TYPE_HU -> contents = contents.replaceAll(Pattern.quote(var.getKey()), target.getWageTypeHu());
+            try {
+                value = field.get(target);
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                throw new IllegalArgumentException(e);
+            }
+
+            if(!ObjectUtils.isEmpty(value)) {
+                contents = contents.replaceAll(Pattern.quote(attachVariableIdentifier(convertToSnakeCase(field.getName()))), value.toString());
             }
         }
 
 //        log.info("REPLACED ###### \n {}", contents);
 
         return contents;
+    }
+
+    /**
+     * String to Variable Converter
+     * @param target
+     * @return
+     */
+    private String attachVariableIdentifier(String target) {
+        return "{{" + target + "}}";
+    }
+
+    /**
+     * Camel Case -> Snake Case Converter
+     * @param target {@link String}
+     * @return {@link String}
+     */
+    private String convertToSnakeCase(String target) {
+        Pattern pattern = Pattern.compile("([a-z])([A-Z])");
+        Matcher matcher = pattern.matcher(target);
+        return matcher.replaceAll(matchResult -> String.format("%s_%s", matchResult.group(1), matchResult.group(2).toLowerCase()));
     }
 }
--- src/main/java/com/samsung/framework/domain/common/Variables.java
+++ src/main/java/com/samsung/framework/domain/common/Variables.java
@@ -9,18 +9,18 @@ import lombok.ToString;
 @Builder
 public class Variables {
 
-    private String name;
-    private String employeeNo;
-    private String hireDateHu;
-    private String hireDateEn;
-    private String contractDateHu;
-    private String contractDateEn;
-    private String jobTitleHu;
-    private String jobTitleEn;
-    private String salaryNo;
-    private String salaryHu;
-    private String salaryEn;
-    private String wageTypeEn;
-    private String wageTypeHu;
+    private String name;            // 이름
+    private String employeeNo;      // 사번
+    private String hireDateHu;      // 입사일_hu
+    private String hireDateEn;      // 입사일_en
+    private String contractDateHu;  // 계약일_hu
+    private String contractDateEn;  // 계약일_en
+    private String jobTitleHu;      // 직무_hu
+    private String jobTitleEn;      // 직무_en
+    private String salaryNo;        // 급여_숫자
+    private String salaryHu;        // 급여_hu
+    private String salaryEn;        // 급여_en
+    private String wageTypeEn;      // 급여형태_en
+    private String wageTypeHu;      // 급여형태_hu
 
 }
