bf46106f7a9f83f13cacda6969f1b2e9fb10540d
Seounggyun <hsk9839@dejay.co.kr>
2024-02-01 09:41:46 +0900

    유저 권한 별 메뉴 리스트 조회 로직 추가

--- src/main/java/com/samsung/framework/service/menu/MenuService.java
+++ src/main/java/com/samsung/framework/service/menu/MenuService.java
@@ -3,8 +3,11 @@ package com.samsung.framework.service.menu;
 import com.samsung.framework.domain.account.SignUpRequest;
 import com.samsung.framework.mapper.authority.AuthorityMapper;
 import com.samsung.framework.mapper.menu.MenuMapper;
+import com.samsung.framework.vo.account.AccountVO;
 import com.samsung.framework.vo.authority.MenuAuthorityVO;
 import com.samsung.framework.vo.menu.MenuVO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -30,37 +33,39 @@ public class MenuService{
      * 대메뉴 리스트 조회
      * @return 대메뉴 List
      */
-    public List<MenuVO> getLgMenuList(){
-        return menuMapper.getLgMenuList();
+    public List<MenuVO> getLgMenuList(AccountVO account){
+        return menuMapper.getLgMenuList(account);
     }
 
     /**
      * 중간 메뉴 리스트 조회
      * @return 중간 메뉴 List
      */
-    public List<MenuVO> getMidMenuList(){
-        return menuMapper.getMidMenuList();
+    public List<MenuVO> getMidMenuList(AccountVO account){
+        return menuMapper.getMidMenuList(account);
     }
 
     /**
      * 소메뉴 리스트 조회
      * @return 소메뉴 List
      */
-    public List<MenuVO> getSmMenuList(){
-        return menuMapper.getSmMenuList();
+    public List<MenuVO> getSmMenuList(AccountVO account){
+        return menuMapper.getSmMenuList(account);
     }
 
-    public Map<String, Object> saveAuthMenu(SignUpRequest signUpRequest){
+    public Map<String, Object> saveAuthMenu(HttpServletRequest request, SignUpRequest signUpRequest){
+        HttpSession session = request.getSession();
+        AccountVO account = (AccountVO) session.getAttribute("loginInfo");
         Map<String, Object> resultMap = new HashMap<>();
-        List<MenuVO> menuList = new ArrayList<>(new ArrayList<>(getLgMenuList()).stream().toList());
+        List<MenuVO> menuList = new ArrayList<>(new ArrayList<>(getLgMenuList(account)).stream().toList());
 
         // 1depth 메뉴 가져오기
-        List<MenuVO> menuMidList = getMidMenuList().stream().toList();
+        List<MenuVO> menuMidList = getMidMenuList(account).stream().toList();
 
         menuList.addAll(menuMidList);
 
         // 2depth 메뉴 가져오기
-        List<MenuVO> menuSmList = getSmMenuList();
+        List<MenuVO> menuSmList = getSmMenuList(account);
         menuList.addAll(menuSmList);
         // menu_seq기준으로 정렬
         Collections.sort(menuList);
